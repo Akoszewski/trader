1d7be9a20ee1760d3a45d7527fae418cc7ada110
ArekK <arkadiusz.koszewski@frey.pl>
2022-06-21 00:41:26 +0200

    Funkcje read data i plot datra

--- script.py
+++ script.py
@@ -14,16 +14,20 @@ class DataPoint:
     def printDataPoint(self):
         print(f"Date: {self.date} open: {self.open} h: {self.high} l: {self.low} close: {self.close}")
 
-data = []
-with open("btc_every_h.csv", "r") as f:
-    rawData = f.readlines()
-    for row in reversed(rawData):
-        row = row.split(",")
-        data.append(DataPoint(row))
+def readData(filename):
+    data = []
+    with open(filename, "r") as f:
+        rawData = f.readlines()
+        for row in reversed(rawData):
+            row = row.split(",")
+            data.append(DataPoint(row))
+    return data
 
-data[0].printDataPoint()
-x = range(len(data))
-prices = [o.open for o in data]
-plt.plot(x, prices)
-plt.show()
+def plotData(data):
+    x = range(len(data))
+    prices = [o.open for o in data]
+    plt.plot(x, prices)
+    plt.show()
 
+data = readData("btc_every_h.csv")
+plotData(data)
