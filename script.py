048e392ad3f8f67344c2fd157464699705344237
ArekK <arkadiusz.koszewski@frey.pl>
2022-06-21 00:35:02 +0200

    Odwrocenie kolejnosci na poprawna i wyswietlenie

--- script.py
+++ script.py
@@ -1,14 +1,15 @@
 from datetime import date
-import csv
+import matplotlib.pyplot as plt
+import numpy as np
 
 # unix, date, symbol, open, high, low, close, VolumeBTC, VolumeUSDT, tradecount
 class DataPoint:
     def __init__(self, dataRow):
         self.date = dataRow[1]
-        self.open = dataRow[3]
-        self.high = dataRow[4]
-        self.low = dataRow[5]
-        self.close = dataRow[6]
+        self.open = float(dataRow[3])
+        self.high = float(dataRow[4])
+        self.low = float(dataRow[5])
+        self.close = float(dataRow[6])
 
     def printDataPoint(self):
         print(f"Date: {self.date} open: {self.open} h: {self.high} l: {self.low} close: {self.close}")
@@ -16,8 +17,13 @@ class DataPoint:
 data = []
 with open("btc_every_h.csv", "r") as f:
     rawData = f.readlines()
-    for row in rawData:
+    for row in reversed(rawData):
         row = row.split(",")
         data.append(DataPoint(row))
 
 data[0].printDataPoint()
+x = range(len(data))
+prices = [o.open for o in data]
+plt.plot(x, prices)
+plt.show()
+
