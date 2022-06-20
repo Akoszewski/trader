6eee6db9a8cc9f1ca8401b6a65189d06530c851c
ArekK <arkadiusz.koszewski@frey.pl>
2022-06-21 01:33:40 +0200

    2 strategie

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 from datetime import date
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
 # unix, date, symbol, open, high, low, close, VolumeBTC, VolumeUSDT, tradecount
 class DataPoint:
@@ -29,5 +30,61 @@ def plotData(data):
     plt.plot(x, prices)
     plt.show()
 
-data = readData("btc_every_h.csv")
-plotData(data)
+class Simulator:
+    def __init__(self, startMoney = 0):
+        self.startMoney = startMoney
+        self.money = self.startMoney
+        self.crypto = 0
+
+    def buy(self, amountOfMoney, price):
+        if self.money >= amountOfMoney:
+            self.money -= amountOfMoney
+            self.crypto += amountOfMoney/price
+    
+    def sell(self, amountOfMoney, price):
+        if self.crypto >= amountOfMoney/price:
+            self.money += amountOfMoney
+            self.crypto -= amountOfMoney/price
+
+    def totalValue(self, price):
+        return self.money + self.crypto * price
+
+# Codzienie kupuje za x dolcow
+def alwaysBuyStrategy(x):
+    totalValues = []
+    simulator = Simulator()
+    for d in data:
+        simulator.buy(x, d.open)
+        totalValues.append(simulator.totalValue(d.open))
+    return totalValues
+
+# Codzienie kupuje za x dolcow
+def alwaysBuyStrategy(startBalance, x):
+    totalValues = []
+    simulator = Simulator(startBalance)
+    for d in data:
+        simulator.buy(x, d.open)
+        totalValues.append(simulator.totalValue(d.open))
+    return totalValues
+
+# Codzienie kupuje za x dolcow
+def randomBuySellStrategy(startBalance, x):
+    totalValues = []
+    simulator = Simulator(startBalance)
+    for d in data:
+        choice = random.randint(0, 1)
+        if choice == 1:
+            simulator.buy(x, d.open)
+        else:
+            simulator.sell(x, d.open)
+        totalValues.append(simulator.totalValue(d.open))
+    return totalValues
+
+data = readData("btc_every_day.csv")
+# plotData(data)
+startBalance = 1000000
+totalValues = randomBuySellStrategy(startBalance, 100)
+print(f"Na koniec masz: {round(totalValues[-1])} dol z zainwestowanych {startBalance}")
+
+plt.plot(totalValues)
+plt.show()
